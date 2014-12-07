dea4a85c9f231fdd3591973de37e6946d66b930a
aj701 <ajlevine@me.com>
2014-12-07 02:06:45 -0500

    wahtever.

--- sketch_still.js
+++ sketch_still.js
@@ -1,21 +1,22 @@
 function setup() {
-  var negDev = createCanvas(640, 480);
-  negDev.parent('myJS');
-  capture = createCapture(VIDEO);
-  capture.size(640, 480);
-  capture.hide();
-  noStroke();
-  fill(0);
+  createCanvas(800, 632);
+img = loadImage("images/portra.jpg");
+  // capture = createCapture(VIDEO);
+  // capture.size(640, 480);
+  // capture.hide();
+  // noStroke();
+  // fill(0);
 }
 
 function draw() {
-	background(255);
+	image(img,0,0);
+
 	image(capture, 0, 0, 640, 480);
 	filter('GRAY')
 	capture.loadPixels();
 	filter('INVERT');
 }
 
-Caman("#negDev", "images/portra.jpg", function () {
+Caman("#negDev", function () {
   this.brightness(5).render();
 });
