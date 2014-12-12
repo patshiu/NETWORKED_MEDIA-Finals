bea0adcb70c74f7289b8186b4a593b8b7ca1f713
aj701 <ajlevine@me.com>
2014-12-11 22:30:17 -0500

    added some js

--- sketch_still.js
+++ sketch_still.js
@@ -1,22 +1,14 @@
 function setup() {
-  createCanvas(800, 632);
-img = loadImage("images/portra.jpg");
-  // capture = createCapture(VIDEO);
-  // capture.size(640, 480);
-  // capture.hide();
-  // noStroke();
-  // fill(0);
+  var negImg = createCanvas(800, 632); //need to autosize for photo?
+  img = loadImage("images/portra400.jpg"); //loading a test image
+  noStroke();
+  fill(0);
 }
 
 function draw() {
-	image(img,0,0);
 
-	image(capture, 0, 0, 640, 480);
-	filter('GRAY')
-	capture.loadPixels();
-	filter('INVERT');
-}
-
-Caman("#negDev", function () {
+	Caman("negImg", function () {
   this.brightness(5).render();
 });
+
+}
