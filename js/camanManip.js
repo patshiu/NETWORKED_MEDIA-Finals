3a00d772dde6e37aca6b2f118b7fc853f8cc9d34
patshiu <pat@gqpbj.com>
2014-12-21 14:46:12 -0500

    Brightness indicator now works.

--- js/camanManip.js
+++ js/camanManip.js
@@ -2,21 +2,28 @@ console.log("PRINT ME. ");
 
 var brightnessSlider = document.getElementById("slider1");
 var brightnessVal = document.getElementById("slider1").value;
+var brightnessDisplay = document.getElementById("currentVal");
 
 
 brightnessSlider.onchange = function () {
 	brightnessVal = document.getElementById("slider1").value;
 	console.log(brightnessVal);
+	brightnessDisplay.innerHTML = brightnessVal; 
 	Caman("#negImg", "../images/portra400.jpg", editImg );
 }
 
+
 //Hard link — http://104.131.178.129:3001/neg/images/portra400.jpg
 
+window.onload = function () {
+	Caman("#negImg", "../images/portra400.jpg", invert );
+	brightnessDisplay.innerHTML = "0"	; 
+}
 
-Caman("#negImg", "../images/portra400.jpg", invert );
 
 
-function invert () {
+
+function	 invert () {
 	this.invert();
 	this.render();
 }
@@ -36,4 +43,4 @@ function editImg () {
 Caman("#idk", function () {
   // manipulate image here
   this.brightness(5).render();
-});
+});this.invert();
