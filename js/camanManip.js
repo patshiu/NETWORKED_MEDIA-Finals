a7db0c6be5d62d3e602fa0de5de79a98759e93c1
patshiu <pat@gqpbj.com>
2014-12-21 23:00:09 -0500

    All working except for shadows, highlights and sharpness.

--- js/camanManip.js
+++ js/camanManip.js
@@ -16,55 +16,137 @@ var allControls = document.getElementsByClassName("controlBox");
 //create an array of all the elements of class "controlBtn"
 var allBtns = document.getElementsByClassName("controlBtn");
 
+//set up the sliders
+var temperatureSlider = document.getElementById("temperatureSlider");
+var hueSlider = document.getElementById("hueSlider");
+var exposureSlider = document.getElementById("exposureSlider");
+var contrastSlider = document.getElementById("contrastSlider");
+var shadowsSlider = document.getElementById("shadowsSlider");
+var highlightsSlider = document.getElementById("highlightsSlider");
+var claritySlider = document.getElementById("claritySlider");
+var gammaSlider = document.getElementById("gammaSlider");
+var sharpnessSlider = document.getElementById("sharpnessSlider");
 
-var brightnessSlider = document.getElementById("slider1");
-var brightnessVal = document.getElementById("slider1").value;
-var brightnessDisplay = document.getElementById("currentVal");
+
+//set up the value displays
+var currentTemperatureVal = document.getElementById("currentTemperatureVal");
+var currentHueVal = document.getElementById("currentHueVal");
+var currentExposureVal = document.getElementById("currentExposureVal");
+var currentContrastVal = document.getElementById("currentContrastVal");
+var currentShadowsVal = document.getElementById("currentShadowsVal");
+var currentHighlightsVal = document.getElementById("currentHighlightsVal");
+var currentClarityVal = document.getElementById("currentClarityVal");
+var currentGammaVal = document.getElementById("currentGammaVal");
+var currentSharpnessVal = document.getElementById("currentSharpnessVal");
 
 
-
-brightnessSlider.onchange = function () {
-	brightnessVal = document.getElementById("slider1").value;
-	console.log(brightnessVal);
-	brightnessDisplay.innerHTML = brightnessVal; 
+//Temperature
+temperatureSlider.onchange = function () {
+	temperatureVal = document.getElementById("temperatureSlider").value;
+	console.log(temperatureVal);
+	currentTemperatureVal.innerHTML = temperatureVal; 
 	Caman("#negImg", "../images/portra400.jpg", editImg );
 };
+
+
+//Hue
+hueSlider.onchange = function () {
+	hueVal = document.getElementById("hueSlider").value;
+	console.log(hueVal);
+	currentHueVal.innerHTML = hueVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Exposure
+exposureSlider.onchange = function () {
+	exposureVal = document.getElementById("exposureSlider").value;
+	console.log(exposureVal);
+	currentExposureVal.innerHTML = exposureVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Contrast
+contrastSlider.onchange = function () {
+	contrastVal = document.getElementById("contrastSlider").value;
+	console.log(contrastVal);
+	currentContrastVal.innerHTML = contrastVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Shadows
+shadowsSlider.onchange = function () {
+	shadowsVal = document.getElementById("shadowsSlider").value;
+	console.log(shadowsVal);
+	currentShadowsVal.innerHTML = shadowsVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Highlights
+highlightsSlider.onchange = function () {
+	highlightsVal = document.getElementById("highlightsSlider").value;
+	console.log(highlightsVal);
+	currentHighlightsVal.innerHTML = highlightsVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Clarity
+claritySlider.onchange = function () {
+	clarityVal = document.getElementById("claritySlider").value;
+	console.log(clarityVal);
+	currentClarityVal.innerHTML = clarityVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Gamma
+gammaSlider.onchange = function () {
+	gammaVal = document.getElementById("gammaSlider").value;
+	console.log(gammaVal);
+	currentGammaVal.innerHTML = gammaVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+//Sharpness
+sharpnessSlider.onchange = function () {
+	sharpnessVal = document.getElementById("sharpnessSlider").value;
+	console.log(sharpnessVal);
+	currentSharpnessVal.innerHTML = sharpnessVal; 
+	Caman("#negImg", "../images/portra400.jpg", editImg );
+};
+
+
+
 
 
 //Hard link — http://104.131.178.129:3001/neg/images/portra400.jpg
 
 window.onload = function () {
 	Caman("#negImg", "../images/portra400.jpg", plainInvert );
-	brightnessDisplay.innerHTML = "0"	; 
+	//Hide all sliders except for temperature
+	updateValues();
+	showOnlyControl(0);
+	brightnessDisplay.innerHTML = "0"; 
 	prepareEventHandlers();
 };
 
-function prepareEventHandlers() {
-	//SET UP BUTTONS
-	for (var i = 0; i < allBtns.length; i++){
-		allBtns[i].onclick = function() {
-			var indexOfClicked = allBtns.indexOf(this);
-			//console.log("Control button number ", index , " was clicked");
-			console.log("it worked! This is button number ", indexOfClicked);
-			showOnlyControl(0);
-		};
-	}
+
+
+function makeBtnActive(index) {
+	allBtns[index].childNodes[0].classList.add("active");
 }
 
-function hideAllControls() {
-	for (var i = 0; i < allControls.length; i++){
-		allControls[i].style.display = 'none';
-	}
+function makeBtnInactive(index) {
+	allBtns[index].childNodes[0].classList.remove("active");
 }
 
 function showOnlyControl(index) {
-	var i; 
-	for (i = 0; i < allControls.length; i++){
+	for (var i = 0; i < allControls.length; i++){
 		if (i != index ){
 			allControls[i].style.display = 'none';
+			makeBtnInactive(i);
 			console.log("Set control #", i ," to invisible.");
 		} else {
 			allControls[i].style.display = 'block';
+			makeBtnActive(i);
 			console.log("Set control #", i ," to visible.");
 		}
 		
@@ -83,15 +165,51 @@ function plainInvert() {
 }
 
 
-function editImg () {
+function editImg() {
   // manipulate image here
   this.revert();
   this.newLayer( function () {
 	  this.setBlendingMode("normal");
 	  this.opacity(100);
 	  this.copyParent();
+	  
 	  this.filter.invert();
-	  this.filter.brightness(brightnessVal);
+	  //Temperature
+
+	  //Hue
+	  this.filter.hue(hueVal);
+
+	  //Exposure
+	  this.filter.exposure(exposureVal);
+
+	  //Contrast
+	  this.filter.contrast(contrastVal/2);
+
+	  // //Shadows
+	  
+	  // //Highlights
+
+	  //Clarity
+	  this.filter.vibrance(clarityVal);
+
+	  //Gamma
+	  this.filter.gamma(gammaVal);
+
+	  //Sharpness
+	  //this.filter.sharpen(sharpnessVal);
+
   });
   this.render();
+}
+
+function updateValues() {
+	currentTemperatureVal.innerHTML = temperatureVal; 
+	currentHueVal.innerHTML = hueVal; // 0 - 100 
+	currentExposureVal.innerHTML = exposureVal; // -100 - 100
+	currentContrastVal.innerHTML = contrastVal; // -100 - 100 
+	currentShadowsVal.innerHTML = shadowsVal; // LATER
+	currentHighlightsVal.innerHTML = highlightsVal; // LATER
+	currentClarityVal.innerHTML = clarityVal;//currently mapped to vibrance -100 - 100
+	currentGammaVal.innerHTML = gammaVal; // 0 - 10 
+	currentSharpnessVal.innerHTML = sharpnessVal; // 0 - 100
 }
